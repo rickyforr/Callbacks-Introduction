123daef60d60da62aed51ed7dd65d236b84309ef
rickyforr <richard.forrester@gmail.com>
2017-05-04 21:22:19 +0000

    added forEach loop to findWaldo function

--- findwaldo.js
+++ findwaldo.js
@@ -2,8 +2,9 @@
 
 
 function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === "Waldo") {
+  arr.forEach(function(name, index) {
+
+    if (name === "Waldo") {
             found(i)  // execute callback
 
     }
@@ -13,6 +14,7 @@ function findWaldo(arr, found) {
 function actionWhenFound(index) {
   console.log("Found him at index " + index);
 }
+
 
 findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
 
